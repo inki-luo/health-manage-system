f5a1e8c14771bbe2a3c0b5b6dfc5c6611463cbc6
is0755ei@ed.ritsumei.ac.jp <eniac@inkis-MacBook-Air.local>
2025-10-02 12:27:58 +0900

    Update exercise list view

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -10,16 +10,16 @@ INSERT INTO exercise_types(exercise_type_name) VALUES ('Indoor Rowing');
 INSERT INTO exercise_types(exercise_type_name) VALUES ('Elliptical');
 INSERT INTO exercise_types(exercise_type_name) VALUES ('Core Training');
 
-INSERT INTO exercise VALUES (default, 1, '2025-08-31', 88);
-INSERT INTO exercise VALUES (default, 2, '2025-09-01', 32);
-INSERT INTO exercise VALUES (default, 3, '2025-09-01', 50);
-INSERT INTO exercise VALUES (default, 4, '2025-09-03', 43);
-INSERT INTO exercise VALUES (default, 5, '2025-09-05', 50);
-INSERT INTO exercise VALUES (default, 6, '2025-09-06', 67);
-INSERT INTO exercise VALUES (default, 3, '2025-09-14', 71);
-INSERT INTO exercise VALUES (default, 1, '2025-09-14', 133);
-INSERT INTO exercise VALUES (default, 2, '2025-09-14', 40);
-INSERT INTO exercise VALUES (default, 3, '2025-07-01', 50);
+INSERT INTO exercise VALUES (default, 1, '2025-08-31 22:41', 88);
+INSERT INTO exercise VALUES (default, 2, '2025-09-01 21:04', 32);
+INSERT INTO exercise VALUES (default, 3, '2025-09-01 11:12', 50);
+INSERT INTO exercise VALUES (default, 4, '2025-09-03 05:33', 43);
+INSERT INTO exercise VALUES (default, 5, '2025-09-05 04:29', 50);
+INSERT INTO exercise VALUES (default, 6, '2025-09-06 18:57', 67);
+INSERT INTO exercise VALUES (default, 3, '2025-09-14 23:26', 71);
+INSERT INTO exercise VALUES (default, 1, '2025-09-14 01:54', 133);
+INSERT INTO exercise VALUES (default, 2, '2025-09-14 06:31', 40);
+INSERT INTO exercise VALUES (default, 3, '2025-07-01 06:07', 50);
 
 
 INSERT INTO food_names VALUES (default, 'Salad');
